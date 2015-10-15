ab18ab48195960c69025d8e2874aec31ec59619c
Isaiah Odhner <isaiahodhner@gmail.com>
2015-10-15 11:15:27 -0400

    Select help items

--- src/help.js
+++ src/help.js
@@ -27,10 +27,28 @@ function show_help(){
 		return object;
 	};
 	
+	var $Item = function(text){
+		var $item = $(E("div")).addClass("item").text(text);
+		$item.on("mousedown", function(){
+			$contents.find(".item").removeClass("selected");
+			$item.addClass("selected");
+		});
+		$item.on("click", function(){
+			var $li = $item.parent();
+			if($li.is(".folder")){
+				$contents.find(".folder").not($li).removeClass("expanded");
+				$li.toggleClass("expanded");
+			}
+		});
+		return $item;
+	};
+	
 	var $default_item_li = $(E("li")).addClass("page");
-	$default_item_li.text("Welcome to Help").click(function(e){
+	$default_item_li.append($Item("Welcome to Help").click(function(e){
+		// $contents.find("li").removeClass("selected");
+		// $item_li.addClass("selected");
 		$iframe.attr({src: "help/default.htm"});
-	});
+	}));
 	$contents.append($default_item_li);
 		
 	$.get("help/mspaint.hhc", function(hhc){
@@ -39,15 +57,17 @@ function show_help(){
 			var object = parse_object($(li).children("object"));
 			
 			var $folder_li = $(E("li")).addClass("folder");
-			$folder_li.text(object.Name);
+			$folder_li.append($Item(object.Name));
 			$contents.append($folder_li);
 			
-			$folder_li.on("click", function(e){
-				if($folder_li.is(e.target)){
-					$contents.find(".folder").not($folder_li).removeClass("expanded");
-					$folder_li.toggleClass("expanded");
-				}
-			});
+			// $folder_li.on("click", function(e){
+			// 	if($folder_li.is(e.target)){
+			// 		$contents.find("li").removeClass("selected");
+			// 		$contents.find(".folder").not($folder_li).removeClass("expanded");
+			// 		$folder_li.addClass("selected");
+			// 		$folder_li.toggleClass("expanded");
+			// 	}
+			// });
 			
 			var $folder_items_ul = $(E("ul"));
 			$folder_li.append($folder_items_ul);
@@ -55,11 +75,15 @@ function show_help(){
 			$(li).children("ul").children().each(function(i, li){
 				var object = parse_object($(li).children("object"));
 				var $item_li = $(E("li")).addClass("page");
-				$item_li.text(object.Name).click(function(e){
+				$item_li.append($Item(object.Name).click(function(e){
+					// $contents.find("li").removeClass("selected");
+					// $item_li.addClass("selected");
 					$iframe.attr({src: "help/" + object.Local});
-				});
+				}));
 				$folder_items_ul.append($item_li);
 			});
 		});
 	});
+	
+	// @TODO: keyboard accessability
 }
