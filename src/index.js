591813bf2cb52882332ae1e46eb3245690adf29e
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2018-01-21 18:18:02 +0000

    Small refactoring/tidying

--- src/index.js
+++ src/index.js
@@ -1,70 +1,89 @@
-(function(){
+(function(window, $) {
 	var default_theme = "classic.css";
 	var theme_storage_key = "jspaint theme";
 
-	var href_for = function(theme){
-		return "styles/themes/" + theme;
-	};
-	try{
+	try {
 		var current_theme = localStorage[theme_storage_key];
-	}catch(e){}
+	} catch(e) {
+	}
 
 	current_theme = current_theme || default_theme;
 
-	document.head.appendChild(create_element('link', {
-        'rel': 'stylesheet',
-        'type': 'text/css',
-        'href': href_for(current_theme),
-        'id': 'theme-link'
-    }));
+	var theme_link = create_element("link", {
+		"rel" : "stylesheet",
+		"type" : "text/css",
+		"href" : href_for(current_theme),
+		"id" : "theme-link"
+	});
 
-	var theme_link = document.getElementById("theme-link");
-	var theme_style = document.createElement("style");
+	var theme_style = create_element("style", {});
 
-	self.set_theme = function(theme){
+	document.head.appendChild(theme_link);
+
+	window.set_theme = function(theme) {
 		current_theme = theme;
+
 		var can_probably_refresh_to_switch = true;
-		try{
+
+		try {
 			localStorage[theme_storage_key] = theme;
-		}catch(e){
+		} catch(e) {
 			can_probably_refresh_to_switch = false;
 		}
-		fetch(href_for(theme))
-		.catch(function(err){
-			show_error_message(
-				"Failed to load theme." +
-				(can_probably_refresh_to_switch ? " You can probably reload the app to finish switching themes." : ""),
-				err
-			);
-		})
-		// I'm gonna tell a funny joke, just wait..
-		.then(function(response) {
-			// Q: Why is this a separate step?
-			// A: Because response.text() returns a Promise!
-			// XD: 😂😂😂 WHO DID THIS 😂😂😂😂 XD
-			return response.text();
-		})
+
+		fetch_theme_css(theme, can_probably_refresh_to_switch)
 		.then(function(css) {
-			if(theme_link){
-				theme_link.parentElement.removeChild(theme_link);
-				theme_link = null;
-			}
-			theme_style.textContent = css;
-			document.head.appendChild(theme_style);
-			$(window).triggerHandler("theme-load");
+			replace_existing_theme_with_loaded_css(
+				theme_link,
+				theme_style,
+				css
+			);
 		});
 	};
-	self.get_theme = function(){
+
+	window.get_theme = function() {
 		return current_theme;
 	};
 
-	function create_element(name, attributes) {
-	    var element = document.createElement(name);
-
-	    for (var attribute_name in attributes) {
-	        element.setAttribute(attribute_name, attributes[attribute_name]);
-	    }
-
-	    return element;
+	function href_for(theme) {
+		return "styles/themes/" + theme;
 	}
-})();
+
+	function create_element(name, attributes) {
+		var element = document.createElement(name);
+
+		for (var attribute_name in attributes) {
+			element.setAttribute(attribute_name, attributes[attribute_name]);
+		}
+
+		return element;
+	}
+
+	function fetch_theme_css(theme, can_probably_refresh_to_switch) {
+		var error_message = "Failed to load theme.";
+
+		if (can_probably_refresh_to_switch) {
+			error_message += " You can probably reload the app to finish switching themes.";
+		}
+
+		return fetch(href_for(theme))
+			.catch(function(error_thrown) {
+				show_error_message(error_message, error_thrown);
+			})
+			.then(function(response) {
+				return response.text();
+			});
+	}
+
+	function replace_existing_theme_with_loaded_css(theme_link, theme_style, css) {
+		if (theme_link.parentElement) {
+			theme_link.parentElement.removeChild(theme_link);
+		}
+
+		theme_style.textContent = css;
+
+		document.head.appendChild(theme_style);
+
+		$(window).triggerHandler("theme-load");
+	}
+})(window, jQuery);
