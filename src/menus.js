44d0df85ace7ee792106be09d78a7ac96dfc3b1e
Isaiah Odhner <isaiahodhner@gmail.com>
2018-06-13 04:23:52 +0000

    Update link to my website
    I thought the 1j01.github.io URL would be more future-proof since if I ever lost the domain I could disable or change the custom domain. But I ended up switching away from GitHub pages, and keeping the domain name (for now).

--- src/menus.js
+++ src/menus.js
@@ -390,7 +390,7 @@ var menus = {
 				$msgbox.title("About Paint");
 				$msgbox.$content.html(
 					"<h1><img src='images/icons/32.png'/> JS Paint <small class='version-number' title='Is that a thing? What I mean is, expect bugs!'>Public Alpha</small><hr/></h1>" +
-					"<p>JS Paint is a web-based remake of MS Paint by <a href='https://1j01.github.io/'>Isaiah Odhner</a>.</p>" +
+					"<p>JS Paint is a web-based remake of MS Paint by <a href='https://isaiahodhner.ml/'>Isaiah Odhner</a>.</p>" +
 					"<p>Read about the project and <b>extra features</b> on <a href='https://github.com/1j01/jspaint#readme'>the README</a>.</p>" +
 					"<p>Request features and report bugs <a href='https://github.com/1j01/jspaint/issues'>on GitHub</a> " +
 					"or <a href='mailto:isaiahodhner@gmail.com?subject=JS%20Paint'>by email</a>.</p>"
