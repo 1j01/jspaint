0e3fab7114f632243b70b3e35b2a5eaa56a7c9ca
Isaiah Odhner <isaiahodhner@gmail.com>
2018-06-29 22:10:31 -0400

    Add a little flying Paint

--- help/vaporwave.js
+++ help/vaporwave.js
@@ -3,8 +3,8 @@ var clouds_img = document.createElement("img");
 clouds_img.src = "clouds.jpg";
 var mask_img = document.createElement("img");
 mask_img.src = "cloud-mask.png";
-// var something_img = document.createElement("img");
-// something_img.src = "p_paint.gif";
+var something_img = document.createElement("img");
+something_img.src = "../images/icons/32.png";
 
 var canvas = document.createElement("canvas");
 document.getElementById("background-animation").append(canvas);
@@ -23,6 +23,12 @@ var animate = function () {
 	var x_interval_ms = 19000;
 	var y_interval_ms = 7000;
 	var now = performance.now();
+	if(!(
+		mask_img.complete && mask_img.naturalWidth > 1 &&
+		clouds_img.complete && clouds_img.naturalWidth > 1
+	)){
+		return;
+	}
 	ctx.drawImage(
 		clouds_img,
 		Math.sin(now / x_interval_ms) * x_extent - x_extent,
@@ -30,12 +36,16 @@ var animate = function () {
 		clouds_width,
 		clouds_height
 	);
+	if(something_img.complete && something_img.naturalWidth > 1){
+		let t = now / 5000;
+		ctx.globalAlpha = 0.3 + Math.max(0, Math.sin(-t) * 1);
+		ctx.drawImage(something_img, Math.sin(-t) * canvas.width * 0.7, Math.cos(-t) * 70);
+		ctx.globalAlpha = 1;
+	}
 	ctx.globalCompositeOperation = "screen";
 	ctx.drawImage(mask_img, 0, 0);
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, mask_img.naturalHeight, mask_img.naturalWidth, canvas.height);
 	ctx.fillRect(mask_img.naturalWidth, 0, 50, canvas.height); // for scrollbar
-	// ctx.globalCompositeOperation = "source-over";
-	// ctx.drawImage(something_img, Math.sin(now / 5000) * 501, Math.cos(now / 5000) * 510);
 };
 animate();
