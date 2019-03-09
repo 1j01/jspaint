bdc6d9c3b37cd71e2136a789481cad24ac164986
Isaiah Odhner <isaiahodhner@gmail.com>
2019-03-09 12:29:43 -0500

    eh?

--- src/theme.js
+++ src/theme.js
@@ -10,6 +10,9 @@
 	}
 
 	var theme_link = document.createElement("link");
+	theme_link.addEventListener("load", function(){
+		console.log("ONLOAD");
+	});
 	theme_link.rel = "stylesheet";
 	theme_link.type = "text/css";
 	theme_link.href = href_for(current_theme);
@@ -37,12 +40,17 @@
 			localStorage[theme_storage_key] = theme;
 		} catch(error) {}
 
-		// note not supported on in all browser browzars
-		theme_link.addEventListener("load", function onload(){
-			theme_link.removeEventListener("load", onload);
-			$(window).triggerHandler("theme-load");
-		});
+		// note not supported in all browser browzars
+// 		theme_link.addEventListener("load", function onload(){
+// console.log("onload for new href", theme_link.href);
+// 			theme_link.removeEventListener("load", onload);
+// 			requestAnimationFrame(function(){
+// 				$(window).triggerHandler("theme-load");
+// 				console.log("theme-load");
+// 			});
+// 		});
 		theme_link.href = href_for(theme);
+		$(window).triggerHandler("theme-load");
 	}
 
 	/**
