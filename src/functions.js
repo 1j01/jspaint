1j01/jspaint

commit 953107a65c0f7db6985e0ef8f8ac7a419bf512b6

Automatically dedupe helper layer updates
This way I don't need to think about it.

src/functions.js
@@ -2,9 +2,33 @@
 function update_magnified_canvas_size(){
 	$canvas.css("width", canvas.width * magnification);
 	$canvas.css("height", canvas.height * magnification);
+
+	update_canvas_rect();
+}
+
+function update_canvas_rect() {
+	canvas_bounding_client_rect = canvas.getBoundingClientRect();
+
+	update_helper_layer();
+}
+
+var helper_layer_update_queued;
+function update_helper_layer(){
+	if (helper_layer_update_queued) {
+		// console.log("update_helper_layer - nah, already queued");
+		return;
+	} else {
+		// console.log("update_helper_layer");
+	}
+	helper_layer_update_queued = true;
+	requestAnimationFrame(()=> {
+		helper_layer_update_queued = false;
+		update_helper_layer_immediately();
+	});
 }
 var info_for_updating_pointer; // for updating on scroll or resize, where the mouse stays in the same place but its coordinates in the document change
-function update_helper_layer(e) {
+function update_helper_layer_immediately(e) {
+	// console.log("update helper layer NOW");
 	if (e) {
 		info_for_updating_pointer = {clientX: e.clientX, clientY: e.clientY, devicePixelRatio};
 	}

src/app.js
@@ -174,22 +174,14 @@ $canvas.on("user-resized", function(e, _x, _y, width, height){
 });
 
 $G.on("resize", function(){ // for browser zoom, and in-app zoom of the canvas
-	canvas_bounding_client_rect = canvas.getBoundingClientRect();
-
-	update_helper_layer();
+	update_canvas_rect();
 	update_disable_aa();
 });
 $canvas_area.on("scroll", function() {
-	canvas_bounding_client_rect = canvas.getBoundingClientRect();
-
-	update_helper_layer();
+	update_canvas_rect();
 });
 $canvas_area.on("resize", function() {
 	update_magnified_canvas_size();
-	
-	canvas_bounding_client_rect = canvas.getBoundingClientRect();
-
-	update_helper_layer();
 });
 
 $("body").on("dragover dragenter", function(e){
@@ -626,7 +618,7 @@ var pointer_active = false;
 var pointer_over_canvas = false;
 var update_helper_layer_on_pointermove_active = false;
 $canvas.on("pointerdown", function(e){
-	canvas_bounding_client_rect = canvas.getBoundingClientRect();
+	update_canvas_rect();
 
 	// Quick Undo when there are multiple pointers (i.e. for touch)
 	// see pointermove for other pointer types

src/tools.js
@@ -416,7 +416,6 @@ tools = [{
 		var prev_magnification = magnification;
 		var prospective_magnification = this.getProspectiveMagnification();
 		
-		// TODO: dedupe update_helper_layer
 		set_magnification(prospective_magnification);
 
 		if (magnification > prev_magnification) {
@@ -430,7 +429,7 @@ tools = [{
 			
 			$canvas_area.scrollLeft(scroll_left);
 			$canvas_area.scrollTop(scroll_top);
-			update_helper_layer();
+			$canvas_area.trigger("scroll");
 		}
 	},
 	$options: $choose_magnification

commit af68c6fe00f079e0c2184bbf3a09857cae257f95

Fix dynamic cursor updating on scroll and zoom
(broken in 953107a65c0f7db6985e0ef8f8ac7a419bf512b6)

src/functions.js
@@ -13,7 +13,11 @@ function update_canvas_rect() {
 }
 
 var helper_layer_update_queued;
-function update_helper_layer(){
+var info_for_updating_pointer; // for updating on scroll or resize, where the mouse stays in the same place but its coordinates in the document change
+function update_helper_layer(e){
+	if (e) {
+		info_for_updating_pointer = {clientX: e.clientX, clientY: e.clientY, devicePixelRatio};
+	}
 	if (helper_layer_update_queued) {
 		// console.log("update_helper_layer - nah, already queued");
 		return;
@@ -26,12 +30,8 @@ function update_helper_layer(){
 		update_helper_layer_immediately();
 	});
 }
-var info_for_updating_pointer; // for updating on scroll or resize, where the mouse stays in the same place but its coordinates in the document change
 function update_helper_layer_immediately(e) {
 	// console.log("update helper layer NOW");
-	if (e) {
-		info_for_updating_pointer = {clientX: e.clientX, clientY: e.clientY, devicePixelRatio};
-	}
 	if (info_for_updating_pointer) {
 		var rescale = info_for_updating_pointer.devicePixelRatio / devicePixelRatio;
 		info_for_updating_pointer.clientX *= rescale;