00829ba45d0becd126370a519275ee22a8d00948
Isaiah Odhner <isaiahodhner@gmail.com>
2019-10-29 15:27:49 -0400

    Convert spaces to tabs

--- src/functions.js
+++ src/functions.js
@@ -974,19 +974,19 @@ function try_exec_command(commandId) {
 }
 
 function getSelectionText() {
-    var text = "";
-    var activeEl = document.activeElement;
-    var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-    if (
+	var text = "";
+	var activeEl = document.activeElement;
+	var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+	if (
 		(activeElTagName == "textarea") || (activeElTagName == "input" &&
 		/^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
 		(typeof activeEl.selectionStart == "number")
-    ) {
-        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-    } else if (window.getSelection) {
-        text = window.getSelection().toString();
-    }
-    return text;
+	) {
+		text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+	} else if (window.getSelection) {
+		text = window.getSelection().toString();
+	}
+	return text;
 }
 
 async function edit_copy(execCommandFallback){
--- src/storage.js
+++ src/storage.js
@@ -2,79 +2,79 @@
 // or maybe replace this module with localforage actually
 
 ((() => {
-  var isArray, localStore;
+	var isArray, localStore;
 
-  isArray = Array.isArray || (a => {
-    return ("" + a) !== a && {}.toString.call(a) === "[object Array]";
-  });
+	isArray = Array.isArray || (a => {
+		return ("" + a) !== a && {}.toString.call(a) === "[object Array]";
+	});
 
-  localStore = {
-    get: function(key, callback) {
-      var defaultValue, err, i, item, len, obj, keys, keys_obj;
-      try {
-        if (typeof key === "string") {
-          item = localStorage.getItem(key);
-          if (item) {
-            obj = JSON.parse(item);
-          }
-        } else {
-          obj = {};
-          if (isArray(key)) {
-            keys = key;
-            for (i = 0, len = keys.length; i < len; i++) {
-              key = keys[i];
-              item = localStorage.getItem(key);
-              if (item) {
-                obj[key] = JSON.parse(item);
-              }
-            }
-          } else {
-            keys_obj = key;
-            for (key in keys_obj) {
-              defaultValue = keys_obj[key];
-              item = localStorage.getItem(key);
-              if (item) {
-                obj[key] = JSON.parse(item);
-              } else {
-                obj[key] = defaultValue;
-              }
-            }
-          }
-        }
-      } catch (_error) {
-        err = _error;
-        callback(err);
-        return;
-      }
-      callback(null, obj);
-    },
-    set: function(key, value, callback) {
-      var err, to_set;
-      to_set = {};
-      if (typeof key === "string") {
-        to_set = {
-          [key]: value
-        };
-      } else if (isArray(key)) {
-        throw new TypeError("Cannot set an array of keys (to what?)");
-      } else {
-        to_set = key, callback = value;
-      }
-      for (key in to_set) {
-        value = to_set[key];
-        try {
-          localStorage.setItem(key, JSON.stringify(value));
-        } catch (_error) {
-          err = _error;
-          err.quotaExceeded = err.code === 22 || err.name === "NS_ERROR_DOM_QUOTA_REACHED" || err.number === -2147024882;
-          callback(err);
-          return;
-        }
-      }
-      return callback(null);
-    }
-  };
+	localStore = {
+		get: function(key, callback) {
+			var defaultValue, err, i, item, len, obj, keys, keys_obj;
+			try {
+				if (typeof key === "string") {
+					item = localStorage.getItem(key);
+					if (item) {
+						obj = JSON.parse(item);
+					}
+				} else {
+					obj = {};
+					if (isArray(key)) {
+						keys = key;
+						for (i = 0, len = keys.length; i < len; i++) {
+							key = keys[i];
+							item = localStorage.getItem(key);
+							if (item) {
+								obj[key] = JSON.parse(item);
+							}
+						}
+					} else {
+						keys_obj = key;
+						for (key in keys_obj) {
+							defaultValue = keys_obj[key];
+							item = localStorage.getItem(key);
+							if (item) {
+								obj[key] = JSON.parse(item);
+							} else {
+								obj[key] = defaultValue;
+							}
+						}
+					}
+				}
+			} catch (_error) {
+				err = _error;
+				callback(err);
+				return;
+			}
+			callback(null, obj);
+		},
+		set: function(key, value, callback) {
+			var err, to_set;
+			to_set = {};
+			if (typeof key === "string") {
+				to_set = {
+					[key]: value
+				};
+			} else if (isArray(key)) {
+				throw new TypeError("Cannot set an array of keys (to what?)");
+			} else {
+				to_set = key, callback = value;
+			}
+			for (key in to_set) {
+				value = to_set[key];
+				try {
+					localStorage.setItem(key, JSON.stringify(value));
+				} catch (_error) {
+					err = _error;
+					err.quotaExceeded = err.code === 22 || err.name === "NS_ERROR_DOM_QUOTA_REACHED" || err.number === -2147024882;
+					callback(err);
+					return;
+				}
+			}
+			return callback(null);
+		}
+	};
 
-  window.storage = localStore;
+	window.storage = localStore;
 
 })).call(this);
