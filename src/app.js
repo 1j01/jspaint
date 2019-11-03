1j01/jspaint

commit 469f13ccc38f89916b68d3e5427e9e92b4c6c5f3

Use a few object param destructurings
$ lebab --replace src/ --transform destruct-param
src/canvas-change.js:
23:  warning  5 different props found, will not transform more than 4  (destruct-param)

Don't destructure events or elements, or (otherwise) lose important context from the variable name.

src/app.js
@@ -430,10 +430,10 @@ set_magnification(1);
 storage.get({
 	width: default_canvas_width,
 	height: default_canvas_height,
-}, (err, values) => {
+}, (err, {width, height}) => {
 	if(err){return;}
-	my_canvas_width = values.width;
-	my_canvas_height = values.height;
+	my_canvas_width = width;
+	my_canvas_height = height;
 	canvas.width = Math.max(1, my_canvas_width);
 	canvas.height = Math.max(1, my_canvas_height);
 	ctx.disable_image_smoothing();

src/electron-injected.js
@@ -125,7 +125,7 @@ window.systemSaveCanvasAs = (canvas, suggestedFileName, savedCallback) => {
 			// TODO: Linux/Unix?? you're not supposed to need file extensions
 			return show_error_message("Missing file extension - try adding .png to the file name");
 		}
-		const formatNameMatched = ((filters.find(filter => filter.extensions.includes(extension))) || {}).name;
+		const formatNameMatched = ((filters.find(({extensions}) => extensions.includes(extension))) || {}).name;
 		if(!formatNameMatched){
 			return show_error_message(`Can't save as *.${extension} - try adding .png to the file name`);
 		}

src/tool-options.js
@@ -99,7 +99,7 @@ const $ChooseShapeStyle = () => {
 			{stroke: true, fill: true},
 			{stroke: false, fill: true}
 		],
-		(a, is_chosen) => {
+		({stroke, fill}, is_chosen) => {
 			const sscanvas = make_canvas(39, 21);
 			const ssctx = sscanvas.ctx;
 			
@@ -107,7 +107,7 @@ const $ChooseShapeStyle = () => {
 			let b = 5;
 			ssctx.fillStyle = is_chosen ? "#fff" : "#000";
 			
-			if(a.stroke){
+			if(stroke){
 				// just using a solid rectangle for the stroke
 				// so as not to have to deal with the pixel grid with strokes
 				ssctx.fillRect(b, b, sscanvas.width-b*2, sscanvas.height-b*2);
@@ -117,7 +117,7 @@ const $ChooseShapeStyle = () => {
 			b += 1;
 			ssctx.fillStyle = "#777";
 			
-			if(a.fill){
+			if(fill){
 				ssctx.fillRect(b, b, sscanvas.width-b*2, sscanvas.height-b*2);
 			}else{
 				ssctx.clearRect(b, b, sscanvas.width-b*2, sscanvas.height-b*2);
@@ -125,11 +125,11 @@ const $ChooseShapeStyle = () => {
 			
 			return sscanvas;
 		},
-		a => {
-			$chooser.stroke = a.stroke;
-			$chooser.fill = a.fill;
+		({stroke, fill}) => {
+			$chooser.stroke = stroke;
+			$chooser.fill = fill;
 		},
-		a => $chooser.stroke === a.stroke && $chooser.fill === a.fill
+		({stroke, fill}) => $chooser.stroke === stroke && $chooser.fill === fill
 	).addClass("choose-shape-style");
 	
 	$chooser.fill = false;
@@ -168,10 +168,10 @@ const $choose_brush = $Choose(
 		render_brush(cbcanvas.ctx, shape, size);
 		
 		return cbcanvas;
-	}, o => {
-		brush_shape = o.shape;
-		brush_size = o.size;
-	}, o => brush_shape === o.shape && brush_size === o.size
+	}, ({shape, size}) => {
+		brush_shape = shape;
+		brush_size = size;
+	}, ({shape, size}) => brush_shape === shape && brush_size === size
 ).addClass("choose-brush");
 
 const $choose_eraser_size = $Choose(

commit 5cb0b20854a021dca45444ef36d37e2e3f5e2d39

Fix overzealous destructuring
Broken in https://github.com/1j01/jspaint/commit/469f13ccc38f89916b68d3e5427e9e92b4c6c5f3

src/app.js
@@ -439,10 +439,10 @@ set_magnification(default_magnification);
 storage.get({
 	width: default_canvas_width,
 	height: default_canvas_height,
-}, (err, {width, height}) => {
+}, (err, stored_values) => {
 	if(err){return;}
-	my_canvas_width = width;
-	my_canvas_height = height;
+	my_canvas_width = stored_values.width;
+	my_canvas_height = stored_values.height;
 	canvas.width = Math.max(1, my_canvas_width);
 	canvas.height = Math.max(1, my_canvas_height);
 	ctx.disable_image_smoothing();