c6f3d7b01787451bfdfb0943a73629eeca568f02
Isaiah Odhner <isaiahodhner@gmail.com>
2019-12-14 16:24:04 -0500

    Update undoable name when a Stamp becomes a Smear

--- src/functions.js
+++ src/functions.js
@@ -1100,6 +1100,10 @@ function make_or_update_undoable(undoable_meta, undoable_action) {
 		current_history_node.selection_image_data = selection && selection.canvas.ctx.getImageData(0, 0, selection.canvas.width, selection.canvas.height);
 		current_history_node.selection_x = selection && selection.x;
 		current_history_node.selection_y = selection && selection.y;
+		if (undoable_meta.update_name) {
+			current_history_node.name = undoable_meta.name;
+		}
+		$G.triggerHandler("history-update"); // update history view
 	} else {
 		undoable(undoable_meta, undoable_action);
 	}
