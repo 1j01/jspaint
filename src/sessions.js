b8b90be87e93f447ceeb2e40e22081eb77d3b7d8
Isaiah Odhner <isaiahodhner@gmail.com>
2019-12-14 18:04:58 -0500

    Fix switching between local and multi-user sessions with the same ID

--- src/sessions.js
+++ src/sessions.js
@@ -509,8 +509,10 @@
 			}else if(!session_id.match(/[-0-9A-Za-z\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02af\u1d00-\u1d25\u1d62-\u1d65\u1d6b-\u1d77\u1d79-\u1d9a\u1e00-\u1eff\u2090-\u2094\u2184-\u2184\u2488-\u2490\u271d-\u271d\u2c60-\u2c7c\u2c7e-\u2c7f\ua722-\ua76f\ua771-\ua787\ua78b-\ua78c\ua7fb-\ua7ff\ufb00-\ufb06]+/)){
 				log("Invalid session ID; it must consist of 'alphanumeric-esque' characters");
 				end_current_session();
-			}else if(current_session && current_session.id === session_id){
-				// @TODO: Handle switching between local and collaborative sessions of the same ID
+			}else if(
+				current_session && current_session.id === session_id && 
+				local === (current_session instanceof LocalSession)
+			){
 				log("Hash changed but the session ID is the same");
 			}else{
 				// @TODO: Ask if you want to save before starting a new session
