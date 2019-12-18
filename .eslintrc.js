64b86920abf1718c01ca24835bef992a22e5b2ca
Isaiah Odhner <isaiahodhner@gmail.com>
2019-12-18 00:45:35 -0500

    Add disabled config for finding some errors
    What I should really do tho is just concat all my source files and then run eslint on that.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,112 @@ module.exports = {
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "ClipboardItem": "readonly",
+
+        // libraries
+        "$": "readonly",
+        "jQuery": "readonly",
+        "libtess": "readonly",
+        "firebase": "readonly",
+        "GIF": "readonly",
+        "saveAs": "readonly",
+
+        /*
+        // const
+        "TAU": "writable",
+        "is_pride_month": "writable",
+        "default_tool": "writable",
+        "default_canvas_width": "writable",
+        "default_canvas_height": "writable",
+
+        // global state: options
+        "brush_shape": "writable",
+        "brush_size": "writable",
+        "pencil_size": "writable",
+        "eraser_size": "writable",
+        "airbrush_size": "writable",
+        "stroke_size": "writable",
+        "tool_transparent_mode": "writable",
+        "magnification": "writable",
+        "transparency": "writable",
+        "aliasing": "writable",
+        "monochrome": "writable",
+        "colors": "writable",
+        "palette": "writable",
+        "polychrome_palette": "writable",
+        "monochrome_palette": "writable",
+        // global state: history
+        "undos": "writable",
+        "redos": "writable",
+        "current_history_node": "writable",
+        "root_history_node": "writable",
+        "history_node_to_cancel_to": "writable",
+        // global state
+        "selection": "writable",
+        "textbox": "writable",
+        "pointer": "writable", // bad
+        "pointer_start": "writable",
+        "pointer_previous": "writable",
+        "pointers": "writable",
+        "saved": "writable",
+        "file_name": "writable",
+        "document_file_path": "writable",
+        "selected_tool": "writable",
+        "selected_tools": "writable",
+        "default_tool": "writable",
+        "shift": "writable",
+        "ctrl": "writable",
+        "my_canvas_width": "writable",
+        "my_canvas_height": "writable",
+        "pointer": "writable",
+        // references that may contain state in some ways
+        "canvas": "writable", // really bad
+        "ctx": "writable", // really bad
+        "helper_layer": "writable",
+        "menus": "writable",
+        "tools": "writable",
+        "extra_tools": "writable",
+        "$G": "writable",
+        "$canvas": "writable",
+        "$canvas_area": "writable",
+        "$top": "writable",
+        "$bottom": "writable",
+        "$left": "writable",
+        "$right": "writable",
+        "$toolbox": "writable",
+        "$colorbox": "writable",
+        "$MenuBar": "writable",
+        "$ToolBox": "writable",
+        "$ColorBox": "writable",
+        "$Window": "writable",
+        "$FormWindow": "writable",
+        "$Handles": "writable",
+        "OnCanvasSelection": "writable",
+        "OnCanvasTextBox": "writable",
+        
+        // functions
+        "E": "writable",
+        "make_canvas": "writable",
+        "debounce": "writable",
+        "memoize_synchronous_function": "writable",
+        "memoize_synchronous_function_with_limit": "writable",
+        "invert_rgb": "off",
+        "get_theme": "writable",
+        "get_tool_by_name": "writable",
+        "redo": "writable",
+        "undo": "writable",
+        "undoable": "writable",
+        "cancel": "writable",
+        "deselect": "writable",
+        "update_helper_layer": "writable",
+        "show_error_message": "writable",
+        "to_canvas_coords": "writable",
+        "update_title": "writable",
+        "get_help_folder_icon": "writable",
+        "get_icon_for_tool": "writable",
+        "draw_polygon": "writable",
+        */
     },
     "parserOptions": {
         "ecmaVersion": 2018
@@ -14,5 +119,10 @@ module.exports = {
     "rules": {
         "no-undef": 0, // FOR NOW OKAY? there are just tons of globals at the moment
         "no-unused-vars": 0, // ditto
+        /*
+        "no-undef": 0,
+        // "no-unused-vars": 0,
+        "no-redeclare": 0,
+        */
     }
 };
