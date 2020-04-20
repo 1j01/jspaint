1j01/jspaint

commit 561d089f4500515f349c0e6ca9d7d551d399e582

Use helper layer for previewing shape tools
- For Multitools, makes the tools render on top of each other like you'd probably expect.
- May improve multi-user experience slightly

src/tools.js
@@ -732,9 +732,6 @@ window.tools = [{
 			const d = Math.sqrt(dx*dx + dy*dy);
 			if(d < 4.1010101 && dt < 250){ // arbitrary 101 (TODO: find correct value (or formula))
 				this.complete(ctx);
-				// Release the pointer to prevent this.paint()
-				// being called and clearing the canvas
-				$canvas.trigger("pointerup");
 			}else{
 				// Add the point
 				this.points.push({x, y});
@@ -943,6 +940,32 @@ tools.forEach((tool)=> {
 			if(!pointer_has_moved) { return; }
 
 			draw_selection_box(ctx, rect_x, rect_y, rect_width, rect_height, scale, translate_x, translate_y);
+		};
+	}
+	if (tool.shape) {
+		tool.shape_canvas = null;
+		tool.pointerdown = ()=> {
+			tool.shape_canvas = make_canvas(canvas.width, canvas.height);
+		};
+		tool.paint = ()=> {
+			tool.shape_canvas.ctx.clearRect(0, 0, tool.shape_canvas.width, tool.shape_canvas.height);
+			tool.shape_canvas.ctx.fillStyle = ctx.fillStyle;
+			tool.shape_canvas.ctx.strokeStyle = ctx.strokeStyle;
+			tool.shape(tool.shape_canvas.ctx, pointer_start.x, pointer_start.y, pointer.x-pointer_start.x, pointer.y-pointer_start.y);
+		};
+		tool.pointerup = ()=> {
+			if(!tool.shape_canvas){ return; }
+			ctx.drawImage(tool.shape_canvas, 0, 0);
+			tool.shape_canvas = null;
+		};
+		tool.drawPreviewUnderGrid = (ctx, x, y, grid_visible, scale, translate_x, translate_y)=> {
+			if(!pointer_active){ return; }
+			if(!tool.shape_canvas){ return; }
+
+			ctx.scale(scale, scale);
+			ctx.translate(translate_x, translate_y);
+
+			ctx.drawImage(tool.shape_canvas, 0, 0);
 		};
 	}
 });

src/app.js
@@ -513,19 +513,6 @@ function update_fill_and_stroke_colors_and_lineWidth(selected_tool) {
 function tool_go(selected_tool, event_name){
 	update_fill_and_stroke_colors_and_lineWidth(selected_tool);
 
-	if(selected_tools.length <= 1){
-		if(selected_tool.shape){
-			const previous_imagedata = undos[undos.length-1];
-			if(previous_imagedata){
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-				ctx.putImageData(previous_imagedata, 0, 0);
-			}
-		}
-	}
-	if(selected_tool.shape){
-		selected_tool.shape(ctx, pointer_start.x, pointer_start.y, pointer.x-pointer_start.x, pointer.y-pointer_start.y);
-	}
-
 	if(selected_tool[event_name]){
 		selected_tool[event_name](ctx, pointer.x, pointer.y);
 	}

commit b244faa13ac1a1c9e21ba642907a0905a7a1e095

Fix rectangle tool line width
- Fixes #160
- Broken in https://github.com/1j01/jspaint/commit/561d089f4500515f349c0e6ca9d7d551d399e582

Co-authored-by: Mike Woodman <mwoodman@mitre.org>

src/tools.js
@@ -1017,6 +1017,7 @@ tools.forEach((tool)=> {
 			tool.shape_canvas.ctx.clearRect(0, 0, tool.shape_canvas.width, tool.shape_canvas.height);
 			tool.shape_canvas.ctx.fillStyle = ctx.fillStyle;
 			tool.shape_canvas.ctx.strokeStyle = ctx.strokeStyle;
+			tool.shape_canvas.ctx.lineWidth = ctx.lineWidth;
 			tool.shape(tool.shape_canvas.ctx, pointer_start.x, pointer_start.y, pointer.x-pointer_start.x, pointer.y-pointer_start.y);
 		};
 		tool.pointerup = ()=> {