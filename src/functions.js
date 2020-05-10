8f34478547d6a00d6cb5a3cd094df8819a23bfa8
Isaiah Odhner <isaiahodhner@gmail.com>
2020-05-10 01:09:25 -0400

    Fix split for #load: URLs

--- src/functions.js
+++ src/functions.js
@@ -20,8 +20,17 @@ const exclusive_params = [
 function get_all_url_params() {
 	const params = {};
 	location.hash.replace(/^#/, "").split(/,/).forEach((param_decl)=> {
-		const [param_name, param_value] = param_decl.split(":");
-		params[param_name] = param_value === undefined ? true : decodeURIComponent(param_value);
+		// colon is used in param value for URLs so split(":") isn't good enough
+		const colon_index = param_decl.indexOf(":");
+		if (colon_index === -1) {
+			// boolean value, implicitly true because it's in the URL
+			const param_name = param_decl;
+			params[param_name] = true;
+		} else {
+			const param_name = param_decl.slice(0, colon_index);
+			const param_value = param_decl.slice(colon_index + 1);
+			params[param_name] = decodeURIComponent(param_value);
+		}
 	});
 	for (const [param_name, param_type] of Object.entries(param_types)) {
 		if (param_type === "bool" && !params[param_name]) {
