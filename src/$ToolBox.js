0db633b83fc7eba48d0c2b9213a92c513740cfbb
Isaiah Odhner <isaiahodhner@gmail.com>
2020-05-30 00:14:42 -0400

    Use dark theme icons as fallback for occult theme in eye gaze mode

--- src/$ToolBox.js
+++ src/$ToolBox.js
@@ -21,8 +21,9 @@ function $ToolBox(tools, is_extras){
 		$icon.appendTo($b);
 		const update_css = ()=> {
 			const theme_folder = `images/${get_theme().replace(/\.css/, "")}`;
+			const theme_has_svg = get_theme().match(/classic.css|dark.css/);
 			const use_svg = !theme_dev_blob_url && (
-				(get_theme().match(/classic.css|dark.css/) &&
+				(theme_has_svg &&
 					(window.devicePixelRatio >= 3 || (window.devicePixelRatio % 1) !== 0)
 				) ||
 				$("body").hasClass("eye-gaze-mode")
@@ -30,7 +31,7 @@ function $ToolBox(tools, is_extras){
 			const background_image = theme_dev_blob_url ? (
 				`url(${theme_dev_blob_url})`
 			) : (
-				use_svg ? `url(images/${get_theme() === "dark.css" ? "dark" : "classic"}/tools.svg)` : `url(${theme_folder}/tools.png)`
+				use_svg ? `url(images/${get_theme().match(/dark.css|occult.css/) ? "dark" : "classic"}/tools.svg)` : `url(${theme_folder}/tools.png)`
 			);
 			$icon.css({
 				display: "block",
