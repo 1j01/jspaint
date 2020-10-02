1f02782b8a9fa3e7b7270fc974a40d9ebef987e7
3P1CL33T <61756336+3P1CL33T@users.noreply.github.com>
2020-10-02 07:21:38 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+// Awesome JSPaint
 const {
 	addMatchImageSnapshotPlugin,
 } = require("cypress-image-snapshot/plugin");
