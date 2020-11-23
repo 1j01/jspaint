45b8c533c1ec1d18f9c8ec0151908daca075bab8
Isaiah Odhner <isaiahodhner@gmail.com>
2020-11-23 14:06:35 -0500

    Perf: disable runtime testing of voice commands
    - Disable runtime testing of the speech recognition system,
      measured at between 87 and 653 ms, averaging 413.6 ms across 5 loads.
    
    - Allow it to be re-enabled with a flag:
        localStorage.test_speech_recognition = "true";
    
    - Only run any voice command tests after the page is loaded.
      Disable a test that now fails due to the menus existing,
      expecting/wanting not to match any command interpretation.

--- src/speech-recognition.js
+++ src/speech-recognition.js
@@ -2028,28 +2028,29 @@ function test_speech(input_text, expected) {
 	}
 }
 
-// test_command("select blue", {color: "blue"}); // @FIXME
-test_command("select fill", {tool_name: "Fill With Color"});
-test_command("select text", {tool_name: "Text"});
-test_command("select", {tool_name: "Select"});
-test_speech("free form select", {tool_name: "Free-Form Select"});
-test_speech("lips", {match_text: "ellipse", tool_name: "Ellipse"});
-test_command("", null);
-// test_command("I got you some new books", null);
-test_command("pan view sorthweast", null);
-test_command("1 pixel lines", {size: 1});
-test_command("1 pixel wide lines", {size: 1});
-test_command("set line width to 5", {size: 5});
-// test_command("use medium-small stroke size", {match_text: "use medium-small stroke size", size: NaN});
-test_speech("set line lips to a hundred", {match_text: "set line width to a hundred", size: 100});
-test_command("use stroke size 10 pixels", {size: 10});
-// test_command("use stroke size of 10 pixels", {match_text: "use stroke size of 10 pixels", size: 10});
-test_command("draw a :-)", {sketch_subject: "smiley face"});
-// test_command("draw sample text", {sketch_subject: "sample text"}); // @FIXME
-test_command("end", {type: "stop-drawing"});
-test_command("stop", {type: "stop-drawing"});
-test_command("draw a stop sign", {sketch_subject: "stop sign"});
-$(()=> {
+function test_speech_recognition() {
+	// test_command("select blue", {color: "blue"}); // @FIXME
+	test_command("select fill", {tool_name: "Fill With Color"});
+	test_command("select text", {tool_name: "Text"});
+	test_command("select", {tool_name: "Select"});
+	test_speech("free form select", {tool_name: "Free-Form Select"});
+	test_speech("lips", {match_text: "ellipse", tool_name: "Ellipse"});
+	test_command("", null);
+	// test_command("I got you some new books", null);
+	// test_command("pan view sorthweast", null); // currently opens View menu
+	test_command("1 pixel lines", {size: 1});
+	test_command("1 pixel wide lines", {size: 1});
+	test_command("set line width to 5", {size: 5});
+	// test_command("use medium-small stroke size", {match_text: "use medium-small stroke size", size: NaN});
+	test_speech("set line lips to a hundred", {match_text: "set line width to a hundred", size: 100});
+	test_command("use stroke size 10 pixels", {size: 10});
+	// test_command("use stroke size of 10 pixels", {match_text: "use stroke size of 10 pixels", size: 10});
+	test_command("draw a :-)", {sketch_subject: "smiley face"});
+	// test_command("draw sample text", {sketch_subject: "sample text"}); // @FIXME
+	test_command("end", {type: "stop-drawing"});
+	test_command("stop", {type: "stop-drawing"});
+	test_command("draw a stop sign", {sketch_subject: "stop sign"});
+
 	test_command("pan view southwest", {vector: {x: -1, y: +1}});
 	test_command("pan southeast", {vector: {x: +1, y: +1}});
 	test_command("move view northwest", {vector: {x: -1, y: -1}});
@@ -2063,6 +2064,16 @@ $(()=> {
 	test_command("go up to the left", {vector: {x: -1, y: -1}});
 	test_speech("cool up", {match_text: "go up", vector: {x: 0, y: -1}});
 	test_command("scroll the view southward", {vector: {x: 0, y: +1}});
-});
+
+}
+
+var should_test_speech_recognition = false;
+try {
+	should_test_speech_recognition = localStorage.test_speech_recognition === "true";
+	// eslint-disable-next-line no-empty
+} catch (error) { }
+if (should_test_speech_recognition) {
+	$(test_speech_recognition);
+}
 
 })();
