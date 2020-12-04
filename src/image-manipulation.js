f3952515b474110d082a172e5d3abb312f577330
Isaiah Odhner <isaiahodhner@gmail.com>
2020-12-04 10:33:43 -0500

    Handle failure to get WebGL context

--- src/image-manipulation.js
+++ src/image-manipulation.js
@@ -999,6 +999,11 @@ function draw_grid(ctx, scale) {
 	function initWebGL(canvas) {
 		gl = canvas.getContext('webgl', { antialias: false });
 
+		if (!gl) {
+			show_error_message("Failed to get WebGL context. You may need to refresh the web page, or restart your computer.");
+			return;
+		}
+
 		window.WEBGL_lose_context = gl.getExtension("WEBGL_lose_context");
 		
 		const program = createShaderProgram();
