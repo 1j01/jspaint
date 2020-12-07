227626dba5e32224e43dc850cda49885948bede4
Isaiah Odhner <isaiahodhner@gmail.com>
2020-12-07 12:53:31 -0500

    Detect more collisions (and change outcome)
    This also changes the localization of "Colors" from "색" to "색상", perhaps among other things. It's hard to view the diff, and I don't know enough about Korean to decide which is better. Probably it should be different in different places, since there are multiple translations, and that will be a separate feature to introduce.

--- localization/preprocess.js
+++ localization/preprocess.js
@@ -17,6 +17,7 @@ could do this in Node.js but whatever
 const fs = require("fs");
 const parse_rc_file = require("./parse-rc-file");
 const base_lang = "en";
+const has_hotkey = str => str.match(/&(\w)/);
 const remove_hotkey = str => str.replace(/&(\w)/, "$1").replace(/\s?\(.\)/, "");
 const remove_ellipsis = str => str.replace("...", "");
 const get_strings = (lang)=> {
@@ -27,6 +28,13 @@ const base_strings = get_strings(base_lang);
 for (const target_lang of ["ko"]) {
 	const target_strings = get_strings(target_lang);
 	const localizations = {};
+	const add_localization = (base_string, target_string)=> {
+		if (localizations[base_string] && localizations[base_string] !== target_string) {
+			console.warn(`Collision for '${base_string}' : '${localizations[base_string]}' vs '${target_string}'`);
+		} else {
+			localizations[base_string] = target_string;
+		}
+	};
 	const add_localizations = (base_strings, target_strings)=> {
 		for (let i = 0; i < target_strings.length; i++) {
 			const base_string = base_strings[i];
@@ -39,13 +47,11 @@ for (const target_lang of ["ko"]) {
 						target_string.split(/\t/)
 					);
 				} else {
-					if (localizations[base_string] && localizations[base_string] !== target_string) {
-						console.warn(`Collision for '${base_string}' : '${localizations[base_string]}' vs '${target_string}'`);
+					add_localization(base_string, target_string);
+					add_localization(remove_ellipsis(base_string), remove_ellipsis(target_string));
+					if (has_hotkey(base_string)) {
+						add_localization(remove_hotkey(base_string), remove_hotkey(target_string));
 					}
-					localizations[base_string] = target_string;
-					// this can cause more collisions @TODO detect
-					localizations[remove_hotkey(base_string)] = remove_hotkey(target_string);
-					localizations[remove_ellipsis(base_string)] = remove_ellipsis(target_string);
 				}
 			}
 		}
