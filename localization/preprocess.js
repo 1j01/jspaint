74932b548a17545682d104fe1f3a23eb2d0f8a10
Isaiah Odhner <isaiahodhner@gmail.com>
2020-12-12 19:16:08 -0500

    Automate concatenating rc files
    - localization/ko/localizations.js changes should just be reorderings.
    - No longer including "combined.rc" files in source control, since the combination is just done in memory.

--- localization/preprocess.js
+++ localization/preprocess.js
@@ -1,31 +1,26 @@
-/*
-first i did this:
-
- 2020-12-06 20:08:31 ⌚  ironsmith in ~/Projects/jspaint/localization/ko
-± |localization ↑1 {5} ?:1 ✗| → find -name '*.rc' -exec cat {} \; > combined.rc
-
- 2020-12-06 20:08:54 ⌚  ironsmith in ~/Projects/jspaint/localization/ko
-± |localization ↑1 {5} ?:1 ✗| → cd ../en
-
- 2020-12-06 20:16:51 ⌚  ironsmith in ~/Projects/jspaint/localization/en
-± |localization ↑2 {5} ?:2 ✗| → find -name '*.rc' -exec cat {} \; > combined.rc
-
-and removed U+FEFF ZERO WIDTH NO-BREAK SPACE that seemed to get introduced between files
-could do this in Node.js but whatever
-*/
-
 const fs = require("fs");
+const glob = require("glob");
 const parse_rc_file = require("./parse-rc-file");
+
 const base_lang = "en";
+const available_langs = fs.readdirSync(__dirname).filter((dir)=> dir.match(/^\w+(-\w+)?$/));
+const target_langs = available_langs.filter((lang)=> lang !== base_lang);
+
 const has_hotkey = str => str.match(/&(\w)/);
 const remove_hotkey = str => str.replace(/&(\w)/, "$1").replace(/\s?\(.\)/, "");
 const remove_ellipsis = str => str.replace("...", "");
+
 const get_strings = (lang)=> {
-	const rc_file_text = fs.readFileSync(`${lang}/combined.rc`, "utf16le");
-	return parse_rc_file(rc_file_text);
+	// TODO: Parse separately in case that matters. (Don't want syntax errors spilling over between files, do we?)
+	const rc_files_text = glob.sync(`${__dirname}/${lang}/**/*.rc`).map(
+		(rc_file)=> fs.readFileSync(rc_file, "utf16le")
+	).join("\n").replace(/\ufeff/g, "");
+	// console.log("\\ufeff index", rc_files_text.indexOf("\ufeff"));
+	return parse_rc_file(rc_files_text);
 };
+
 const base_strings = get_strings(base_lang);
-for (const target_lang of ["ko"]) {
+for (const target_lang of target_langs) {
 	const target_strings = get_strings(target_lang);
 	const localizations = {};
 	const add_localization = (base_string, target_string)=> {
@@ -60,5 +55,5 @@ for (const target_lang of ["ko"]) {
 	};
 	add_localizations(base_strings, target_strings);
 	const js = `loaded_localizations("${target_lang}", ${JSON.stringify(localizations, null, "\t")});\n`;
-	fs.writeFileSync(`${target_lang}/localizations.js`, js);
+	fs.writeFileSync(`${__dirname}/${target_lang}/localizations.js`, js);
 }
