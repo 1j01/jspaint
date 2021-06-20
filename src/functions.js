29bffc3a474d8f93673ba4fef746f85ed6386e92
Isaiah Odhner <isaiahodhner@gmail.com>
2021-06-19 22:40:53 -0400

    Note farbling breakage

--- src/functions.js
+++ src/functions.js
@@ -2010,6 +2010,12 @@ function has_any_transparency(ctx) {
 }
 
 function detect_monochrome(ctx) {
+	// Note: Brave browser, and DuckDuckGo Privacy Essentials browser extension
+	// implement a privacy technique known as "farbling", which breaks this code.
+	// (I've implemented workarounds in many places, but not here yet.)
+	// This function currently returns the set of one or two colors if applicable,
+	// and things outside would need to be changed to handle a "near-monochrome" state.
+
 	const id = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 	const pixelArray = new Uint32Array(id.data.buffer); // to access as whole pixels (for greater efficiency & simplicity)
 	// Note: values in pixelArray may be different on big endian vs little endian machines.
