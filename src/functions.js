c761d70001641af78acd560da0cbd5c02888cb01
Isaiah Odhner <isaiahodhner@gmail.com>
2021-12-07 15:44:45 -0500

    Fix Ctrl+F+F to toggle View Bitmap off (event was duplicately handled)

--- src/functions.js
+++ src/functions.js
@@ -2384,6 +2384,10 @@ function view_bitmap() {
 			return; // Chrome sends an F keydown with repeat=false if you release Ctrl before F, while repeating.
 			// This is a slightly overkill, and slightly overzealous workaround (can ignore one normal F before handling F as exit)
 		}
+		// Prevent also toggling View Bitmap on while toggling off, with Ctrl+F+F.
+		// That is, if you hold Ctrl and press F twice, the second F should close View Bitmap and not reopen it immediately.
+		// This relies on the keydown handler handling event.defaultPrevented (or isDefaultPrevented() if it's using jQuery)
+		event.preventDefault();
 		// Note: in mspaint, Esc is the only key that DOESN'T close the bitmap view,
 		// but it also doesn't do anything else — other than changing the cursor. Stupid.
 		cleanup_bitmap_view();
