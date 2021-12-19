1de7cf2b44e9f3a4f01d5bcb1140e545476d42ec
Isaiah Odhner <isaiahodhner@gmail.com>
2021-12-19 03:04:34 -0500

    Fix hiding/showing Thumbnail

--- src/functions.js
+++ src/functions.js
@@ -482,28 +482,38 @@ function toggle_thumbnail() {
 			$thumbnail_window.maximize = () => { }; // @TODO: disable maximize with an option
 			new ResizeObserver((entries) => {
 				const entry = entries[0];
+				let width, height;
 				if ("devicePixelContentBoxSize" in entry) {
 					// console.log("devicePixelContentBoxSize", entry.devicePixelContentBoxSize);
 					// Firefox seems to support this, although I can't find any documentation that says it should
 					// I can't find an implementation bug or anything.
 					// So I had to disable this case to test the fallback case (in Firefox 94.0)
-					thumbnail_canvas.width = entry.devicePixelContentBoxSize[0].inlineSize;
-					thumbnail_canvas.height = entry.devicePixelContentBoxSize[0].blockSize;
+					width = entry.devicePixelContentBoxSize[0].inlineSize;
+					height = entry.devicePixelContentBoxSize[0].blockSize;
 				} else if ("contentBoxSize" in entry) {
 					// console.log("contentBoxSize", entry.contentBoxSize);
 					// round() seems to line up with what Firefox does for device pixel alignment, which is great.
 					// In Chrome it's blurry at some zoom levels with round(), ceil(), or floor(), but it (documentedly) supports devicePixelContentBoxSize.
-					thumbnail_canvas.width = Math.round(entry.contentBoxSize[0].inlineSize * devicePixelRatio);
-					thumbnail_canvas.height = Math.round(entry.contentBoxSize[0].blockSize * devicePixelRatio);
+					width = Math.round(entry.contentBoxSize[0].inlineSize * devicePixelRatio);
+					height = Math.round(entry.contentBoxSize[0].blockSize * devicePixelRatio);
 				} else {
 					// Safari on iPad doesn't support either of the above as of iOS 15.0.2
-					thumbnail_canvas.width = Math.round(entry.contentRect.width * devicePixelRatio);
-					thumbnail_canvas.height = Math.round(entry.contentRect.height * devicePixelRatio);
+					width = Math.round(entry.contentRect.width * devicePixelRatio);
+					height = Math.round(entry.contentRect.height * devicePixelRatio);
+				}
+				if (width && height) { // If it's hidden, and then shown, it gets a width and height of 0 briefly on iOS. (This would give IndexSizeError in drawImage.)
+					thumbnail_canvas.width = width;
+					thumbnail_canvas.height = height;
 				}
 				update_helper_layer_immediately(); // updates thumbnail (but also unnecessarily the helper layer)
-			}).observe(thumbnail_canvas, { box: ['device-pixel-content-box'] }); // @TODO: does the box make it worse browser support?
+			}).observe(thumbnail_canvas, { box: ['device-pixel-content-box'] });
 		}
 		$thumbnail_window.show();
+		$thumbnail_window.on("close", (e) => {
+			e.preventDefault();
+			$thumbnail_window.hide();
+			show_thumbnail = false;
+		});
 	}
 	// Currently the thumbnail updates with the helper layer. But it's not part of the helper layer, so this is a bit of a misnomer for now.
 	update_helper_layer();
