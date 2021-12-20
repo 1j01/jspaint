f3bd1ea36d0dc61ccc26e6258dd428c1450e2187
Isaiah Odhner <isaiahodhner@gmail.com>
2021-12-20 02:20:24 -0500

    Update comment with alternatives
    I'm giving up on this for now I think. It's good enough.
    Reloading for changing the language is a very infrequent action,
    and reloading for development is frequent but I can live with it.
    Detecting fullscreen is NOT what I want to be working on.

--- src/functions.js
+++ src/functions.js
@@ -1418,11 +1418,26 @@ function exit_fullscreen_if_ios() {
 			}
 		} catch (error) {
 			// not important, just trying to prevent broken fullscreen after refresh
-			// (:fullscreen stops working because it's not "requested by the page" anymore)
-			// alternatively, maybe I could use onbeforeunload,
-			// and alternatively to exiting fullscreen, maybe I could store the fullscreen state,
-			// with a timestamp in localStorage, and use a class instead of :fullscreen (WIP...)
+			// (:fullscreen and document.fullscreenElement stops working because it's not "requested by the page" anymore)
 			// (the fullscreen styling is not generally obtrusive, but it is obtrusive when it DOESN'T work)
+			//
+			// alternatives:
+			// - detect reload-while-fullscreen by storing a timestamp on unload when fullscreen,
+			//   and apply the fullscreen class if timestamp is within a few seconds during load.
+			//   - This doesn't have an answer for detecting leaving fullscreen,
+			//     and if it keeps thinking it's fullscreen, it'll keep storing the timestamp, and get stuck.
+			//     Unless it only stores the timestamp if it knows it's fullscreen? (i.e. page-requested fullscreen)
+			//     Then it would only work for one reload.
+			//     So ideally it would have the below anyway, in which case this would be unnecessary.
+			// - detect fullscreen state without fullscreen API, using viewport size
+			//   - If this is possible, why don't browsers just expose this information in the fullscreen API? :(
+			//   - iPad resets the zoom level when going fullscreen, and then when reloading,
+			//     the zoom level is reset to the user-set zoom level.
+			//     Safari doesn't update devicePixelRatio based on the zoom level,
+			//     and doesn't support ResizeObserver for device pixels.
+			//     It does support https://developer.mozilla.org/en-US/docs/Web/API/Visual_Viewport_API
+			//     though, so maybe something can be done with that.
+			// - prompt to add to homescreen
 		}
 	}
 }
