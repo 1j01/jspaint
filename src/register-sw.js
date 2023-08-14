cef06aa5d82ba89da5c43b127ef0bd86b2b9138c
Isaiah Odhner <isaiahodhner@gmail.com>
2023-08-14 15:27:56 -0400

    uugh, what a mess...

--- src/register-sw.js
+++ src/register-sw.js
@@ -14,28 +14,16 @@ if ('serviceWorker' in navigator) {
 	const wb = new Workbox('/sw.js');
 
 	const showSkipWaitingPrompt = async (event) => {
-		// Assuming the user accepted the update, set up a listener
-		// that will reload the page as soon as the previously waiting
-		// service worker has taken control.
 		wb.addEventListener('controlling', () => {
-			// At this point, reloading will ensure that the current
-			// tab is loaded under the control of the new service worker.
-			// Depending on your web app, you may want to auto-save or
-			// persist transient state before triggering the reload.
-			are_you_sure(() => {
+			if (window.are_you_sure) {
+				are_you_sure(() => {
+					window.location.reload();
+				});
+			} else {
 				window.location.reload();
-			});
+			}
 		});
 
-		// When `event.wasWaitingBeforeRegister` is true, a previously
-		// updated service worker is still waiting.
-		// You may want to customize the UI prompt accordingly.
-
-		// This code assumes your app has a promptForUpdate() method,
-		// which returns true if the user wants to update.
-		// Implementing this is app-specific; some examples are:
-		// https://open-ui.org/components/alert.research or
-		// https://open-ui.org/components/toast.research
 		const updateAccepted = await promptForUpdate();
 
 		if (updateAccepted) {
@@ -43,8 +31,6 @@ if ('serviceWorker' in navigator) {
 		}
 	};
 
-	// Add an event listener to detect when the registered
-	// service worker has installed but is waiting to activate.
 	wb.addEventListener('waiting', (event) => {
 		showSkipWaitingPrompt(event);
 	});
@@ -53,6 +39,14 @@ if ('serviceWorker' in navigator) {
 }
 
 async function promptForUpdate() {
+	if (!window.showMessageBox) {
+		// On about.html, there's no showMessageBox.
+		// I might include it later for better integration with the jspaint embed though.
+		// At any rate, it should be fairly harmless to skip the prompt on this page...
+		// excepppppt for the fact that there's a jspaint embed.
+		// @TODO
+		return true;
+	}
 	const { promise, $window } = showMessageBox({
 		title: localize("Update %1", localize("Paint")),
 		message: localize("Update %1 before proceeding?", localize("Paint")),
