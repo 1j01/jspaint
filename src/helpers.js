0da1108e9e90c7be9cf3daa6b41c0d3c7c2fc0eb
Isaiah Odhner <isaiahodhner@gmail.com>
2024-04-07 02:56:40 -0400

    Clean up unused temporary globals

--- src/helpers.js
+++ src/helpers.js
@@ -425,5 +425,4 @@ export {
 // Temporary globals until all dependent code is converted to ES Modules
 window.$G = $G; // used by app-localization.js
 window.make_canvas = make_canvas; // used by app-state.js, electron-injected.js
-window.get_help_folder_icon = get_help_folder_icon; // unused
 window.get_format_from_extension = get_format_from_extension; // used by electron-injected.js
