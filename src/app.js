1j01/jspaint

commit 567fd6f1ad6331d3c0e6ccb73e1806ed386fe4fb

Implement remaining MS Paint shortcuts
I might be mixing versions of MS Paint here btw, but whatever, I'm only adding shortcuts.

src/app.js
@@ -870,7 +870,9 @@ $G.on("keydown", e => {
 		}
 	}
 	if (
-		// Ctrl+Shift+Y
+		// Ctrl+Shift+Y for history window,
+		// chosen because it's related to the undo/redo shortcuts
+		// and it looks like a branching symbol.
 		(e.ctrlKey || e.metaKey) && e.shiftKey && !e.altKey &&
 		e.key.toUpperCase() === "Y"
 	) {
@@ -932,10 +934,33 @@ $G.on("keydown", e => {
 		if(selection){
 			deselect();
 		}
+	} else if (e.key === "F1") {
+		show_help();
+		e.preventDefault();
 	} else if (e.key === "F4") {
 		redo();
+	// alt+backspace: undo
+	// shift+delete: cut
+	// delete/backspace: delete selection
 	} else if (e.key === "Delete" || e.key === "Backspace") {
-		delete_selection();
+		if (e.key === "Delete" && e.shiftKey) {
+			cut();
+		} else if (e.key === "Backspace" && e.altKey) {
+			undo();
+		} else {
+			delete_selection();
+		}
+		e.preventDefault();
+	// ctrl+insert: copy
+	// shift+insert: paste
+	} else if (e.key === "Insert") {
+		if (e.ctrlKey) {
+			copy();
+			e.preventDefault();
+		} else if (e.shiftKey) {
+			paste();
+			e.preventDefault();
+		}
 	} else if (
 		e.code === "NumpadAdd" ||
 		e.code === "NumpadSubtract" ||
@@ -993,6 +1018,19 @@ $G.on("keydown", e => {
 					return;
 			}
 		}
+		// Ctrl+PageDown: zoom to 400%
+		// Ctrl+PageUp: zoom to 100%
+		// In Chrome and Firefox, these switch to the next/previous tab,
+		// but it's allowed to be overridden in fullscreen in Chrome.
+		if (e.key === "PageDown") {
+			set_magnification(4);
+			e.preventDefault();
+			return;
+		} else if (e.key === "PageUp") {
+			set_magnification(1);
+			e.preventDefault();
+			return;
+		}
 		switch (e.key.toUpperCase()) {
 			case ",": // '<' without Shift
 			case "<":
@@ -1026,9 +1064,6 @@ $G.on("keydown", e => {
 			case "O":
 				file_open();
 			break;
-			case "N":
-				e.shiftKey ? clear() : file_new();
-			break;
 			case "S":
 				e.shiftKey ? file_save_as() : file_save();
 			break;
@@ -1041,10 +1076,34 @@ $G.on("keydown", e => {
 			case "E":
 				image_attributes();
 			break;
+
+			// These shortcuts are mostly reserved by browsers,
+			// but they are allowed in Electron.
+			// The shortcuts are hidden in the menus (or changed) when not in Electron,
+			// to prevent accidental closing/refreshing.
+			// I'm supporting Alt+<shortcut> here (implicitly) as a workaround (and showing this in the menus in some cases).
+			// Also, note that Chrome allows some shortcuts to be overridden in fullscreen (but showing/hiding the shortcuts would be confusing).
+			case "N":
+				e.shiftKey ? clear() : file_new();
+			break;
+			case "T":
+				$toolbox.toggle();
+			break;
+			case "L": // allowed to override in Firefox
+				$colorbox.toggle();
+			break;
+			case "R":
+				image_flip_and_rotate();
+			break;
+			case "W":
+				image_stretch_and_skew();
+			break;
+
 			default:
 				return; // don't preventDefault
 		}
 		e.preventDefault();
+		// put nothing below! note return above
 	}
 });
 let alt_zooming = false;

src/electron-injected.js
@@ -30,6 +30,7 @@ if(process.platform == "win32" && isPackaged){
 }
 
 window.is_electron_app = true;
+window.electron_is_dev = process.env.ELECTRON_DEBUG === "1" || !isPackaged;
 
 window.setRepresentedFilename = (filePath) => {
 	require("electron").remote.getCurrentWindow().setRepresentedFilename(filePath);

src/menus.js
@@ -8,7 +8,7 @@ window.menus = {
 	[localize("&File")]: [
 		{
 			item: localize("&New"),
-			shortcut: "Ctrl+Alt+N", // Ctrl+N opens a new browser window
+			shortcut: window.is_electron_app ? "Ctrl+N" : "Ctrl+Alt+N", // Ctrl+N opens a new browser window
 			speech_recognition: [
 				"new", "new file", "new document", "create new document", "create a new document", "start new document", "start a new document",
 			],
@@ -63,7 +63,7 @@ window.menus = {
 		MENU_DIVIDER,
 		{
 			item: localize("&Load From URL"),
-			// shortcut: "Ctrl+L",
+			// shortcut: "", // no shortcut: Ctrl+L is taken, and you can paste a URL with Ctrl+V, so it's not really needed
 			speech_recognition: [
 				"load from url",
 				"load from a url",
@@ -155,7 +155,7 @@ window.menus = {
 		},
 		{
 			item: localize("&Print"),
-			shortcut: "Ctrl+P",
+			shortcut: "Ctrl+P", // relies on browser's print shortcut being Ctrl+P
 			speech_recognition: [
 				"print", "send to printer", "show print dialog",
 				"print page", "print image", "print picture", "print drawing",
@@ -207,7 +207,7 @@ window.menus = {
 		MENU_DIVIDER,
 		{
 			item: localize("E&xit"),
-			// shortcut: "Alt+F4", // closes browser window
+			shortcut: window.is_electron_app ? "Alt+F4" : "", // Alt+F4 closes the browser window (in most window managers)
 			speech_recognition: [
 				"exit application", "exit paint", "close paint window",
 			],
@@ -245,7 +245,7 @@ window.menus = {
 		},
 		{
 			item: localize("&Repeat"),
-			shortcut: "F4",
+			shortcut: "F4", // also supported: Ctrl+Shift+Z, Ctrl+Y
 			speech_recognition: [
 				"repeat", "redo",
 			],
@@ -352,7 +352,7 @@ window.menus = {
 	[localize("&View")]: [
 		{
 			item: localize("&Tool Box"),
-			// shortcut: "Ctrl+T", // opens a new browser tab
+			shortcut: window.is_electron_app ? "Ctrl+T" : "", // Ctrl+T opens a new browser tab, Ctrl+Alt+T opens a Terminal in Ubuntu, and Ctrl+Shift+Alt+T feels silly.
 			speech_recognition: [
 				"toggle tool box", "toggle tools box", "toggle toolbox", "toggle tool palette", "toggle tools palette",
 				// @TODO: hide/show
@@ -367,7 +367,7 @@ window.menus = {
 		},
 		{
 			item: localize("&Color Box"),
-			// shortcut: "Ctrl+L", // focuses browser address bar
+			shortcut: "Ctrl+L", // focuses browser address bar, but Firefox and Chrome both allow overriding the default behavior
 			speech_recognition: [
 				"toggle color box", "toggle colors box", "toggle palette", "toggle color palette", "toggle colors palette",
 				// @TODO: hide/show
@@ -412,7 +412,7 @@ window.menus = {
 			submenu: [
 				{
 					item: localize("&Normal Size"),
-					// shortcut: "Ctrl+PgUp", // cycles thru browser tabs
+					shortcut: window.is_electron_app ? "Ctrl+PgUp" : "", // Ctrl+PageUp cycles thru browser tabs in Chrome & Firefox; can be overridden in Chrome in fullscreen only
 					speech_recognition: [
 						"reset zoom", "zoom to normal size",
 						"zoom to 100%", "set zoom to 100%", "set zoom 100%",
@@ -428,7 +428,7 @@ window.menus = {
 				},
 				{
 					item: localize("&Large Size"),
-					// shortcut: "Ctrl+PgDn", // cycles thru browser tabs
+					shortcut: window.is_electron_app ? "Ctrl+PgDn" : "", // Ctrl+PageDown cycles thru browser tabs in Chrome & Firefox; can be overridden in Chrome in fullscreen only
 					speech_recognition: [
 						"zoom to large size",
 						"zoom to 400%", "set zoom to 400%", "set zoom 400%",
@@ -533,7 +533,7 @@ window.menus = {
 		MENU_DIVIDER,
 		{
 			item: localize("&Fullscreen"),
-			shortcut: "F11",
+			shortcut: "F11", // relies on browser's shortcut
 			speech_recognition: [
 				// won't work with speech recognition, needs a user gesture
 			],
@@ -560,7 +560,7 @@ window.menus = {
 		// @TODO: speech recognition: terms that apply to selection
 		{
 			item: localize("&Flip/Rotate"),
-			// shortcut: "Ctrl+R", // reloads browser tab
+			shortcut: (window.is_electron_app && !window.electron_is_dev) ? "Ctrl+R" : "Ctrl+Alt+R", // Ctrl+R reloads the browser tab (or Electron window in dev mode via electron-debug)
 			speech_recognition: [
 				"flip",
 				"rotate",
@@ -572,7 +572,7 @@ window.menus = {
 		},
 		{
 			item: localize("&Stretch/Skew"),
-			// shortcut: "Ctrl+W", // closes browser tab
+			shortcut: window.is_electron_app ? "Ctrl+W" : "Ctrl+Alt+W", // Ctrl+W closes the browser tab
 			speech_recognition: [
 				"stretch", "scale", "resize image",
 				"skew",
@@ -611,7 +611,7 @@ window.menus = {
 		},
 		{
 			item: localize("&Clear Image"),
-			shortcut: looksLikeChrome ? undefined : "Ctrl+Shift+N", // opens incognito window in chrome
+			shortcut: (window.is_electron_app || !looksLikeChrome) ? "Ctrl+Shift+N" : "", // Ctrl+Shift+N opens incognito window in chrome
 			speech_recognition: [
 				"clear image", "clear canvas", "clear picture", "clear page", "clear drawing",
 				// @TODO: erase?

commit e3ef424d61e0621e36adeeaa37070d07cf7f5d29

Fix Shift+Insert, Ctrl+Insert, and Ctrl+Delete shortcuts
Fixes https://github.com/1j01/jspaint/issues/338

These function calls used the wrong names since they were introduced
in https://github.com/1j01/jspaint/commit/567fd6f1ad

src/app.js
@@ -1,7 +1,7 @@
 // @ts-check
 // eslint-disable-next-line no-unused-vars
 /* global airbrush_size:writable, brush_shape:writable, brush_size:writable, button:writable, ctrl:writable, eraser_size:writable, fill_color:writable, fill_color_k:writable, history_node_to_cancel_to:writable, MenuBar:writable, my_canvas_height:writable, my_canvas_width:writable, palette:writable, pencil_size:writable, pointer:writable, pointer_active:writable, pointer_buttons:writable, pointer_over_canvas:writable, pointer_previous:writable, pointer_start:writable, pointer_type:writable, pointers:writable, reverse:writable, shift:writable, stroke_color:writable, stroke_color_k:writable, stroke_size:writable, update_helper_layer_on_pointermove_active:writable */
-/* global copy, current_history_node, cut, default_airbrush_size, default_brush_shape, default_brush_size, default_canvas_height, default_canvas_width, default_eraser_size, default_magnification, default_pencil_size, default_stroke_size, enable_fs_access_api, file_name, get_direction, localize, magnification, main_canvas, main_ctx, remove_hotkey, return_to_tools, selected_colors, selected_tool, selected_tools, selection, showSaveFilePicker, systemHooks, textbox, transparency */
+/* global current_history_node, default_airbrush_size, default_brush_shape, default_brush_size, default_canvas_height, default_canvas_width, default_eraser_size, default_magnification, default_pencil_size, default_stroke_size, enable_fs_access_api, file_name, get_direction, localize, magnification, main_canvas, main_ctx, remove_hotkey, return_to_tools, selected_colors, selected_tool, selected_tools, selection, showSaveFilePicker, systemHooks, textbox, transparency */
 
 import { $ColorBox } from "./$ColorBox.js";
 import { $ToolBox } from "./$ToolBox.js";
@@ -9,7 +9,7 @@ import { Handles } from "./Handles.js";
 // import { get_direction, localize, remove_hotkey } from "./app-localization.js";
 import { default_palette, get_winter_palette } from "./color-data.js";
 import { image_formats } from "./file-format-data.js";
-import { $this_version_news, cancel, change_url_param, clear, confirm_overwrite_capability, delete_selection, deselect, edit_copy, edit_cut, file_new, file_open, file_save, file_save_as, get_tool_by_id, get_uris, image_attributes, image_flip_and_rotate, image_invert_colors, image_stretch_and_skew, load_image_from_uri, make_or_update_undoable, open_from_file, paste, paste_image_from_file, redo, render_history_as_gif, reset_canvas_and_history, reset_file, reset_selected_colors, resize_canvas_and_save_dimensions, resize_canvas_without_saving_dimensions, save_as_prompt, select_all, select_tool, select_tools, set_magnification, show_document_history, show_error_message, show_news, show_resource_load_error_message, toggle_grid, undo, update_canvas_rect, update_disable_aa, update_helper_layer, update_magnified_canvas_size, view_bitmap, write_image_file } from "./functions.js";
+import { $this_version_news, cancel, change_url_param, clear, confirm_overwrite_capability, delete_selection, deselect, edit_copy, edit_cut, edit_paste, file_new, file_open, file_save, file_save_as, get_tool_by_id, get_uris, image_attributes, image_flip_and_rotate, image_invert_colors, image_stretch_and_skew, load_image_from_uri, make_or_update_undoable, open_from_file, paste, paste_image_from_file, redo, render_history_as_gif, reset_canvas_and_history, reset_file, reset_selected_colors, resize_canvas_and_save_dimensions, resize_canvas_without_saving_dimensions, save_as_prompt, select_all, select_tool, select_tools, set_magnification, show_document_history, show_error_message, show_news, show_resource_load_error_message, toggle_grid, undo, update_canvas_rect, update_disable_aa, update_helper_layer, update_magnified_canvas_size, view_bitmap, write_image_file } from "./functions.js";
 import { show_help } from "./help.js";
 import { $G, E, TAU, get_file_extension, get_help_folder_icon, is_discord_embed, make_canvas, to_canvas_coords } from "./helpers.js";
 import { init_webgl_stuff, rotate } from "./image-manipulation.js";
@@ -961,7 +961,7 @@ $G.on("keydown", e => {
 		// shift+delete: cut
 		// delete/backspace: delete selection
 		if (e.key === "Delete" && e.shiftKey) {
-			cut();
+			edit_cut();
 		} else if (e.key === "Backspace" && e.altKey) {
 			undo();
 		} else {
@@ -972,10 +972,10 @@ $G.on("keydown", e => {
 		// ctrl+insert: copy
 		// shift+insert: paste
 		if (e.ctrlKey) {
-			copy();
+			edit_copy();
 			e.preventDefault();
 		} else if (e.shiftKey) {
-			paste();
+			edit_paste();
 			e.preventDefault();
 		}
 	} else if (