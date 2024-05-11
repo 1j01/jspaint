fc59a369fd306bedc31611e6729f4fc2ae9e2cc0
Isaiah Odhner <isaiahodhner@gmail.com>
2024-05-10 21:13:56 -0400

    Fix default file name when saving selection with Edit > Copy To
    This also fixes an error in the Electron app with Edit > Copy To:
    
        Error occurred in handler for 'show-save-dialog': TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received undefined
            at new NodeError (node:internal/errors:371:5)
            at validateString (node:internal/validators:120:11)
            at Object.basename (node:path:752:5)
            at C:\...\jspaint\src\electron-main.js:469:45
            at node:electron/js2c/browser_init:189:579
            at EventEmitter.<anonymous> (node:electron/js2c/browser_init:161:11327)
            at EventEmitter.emit (node:events:526:28) {
          code: 'ERR_INVALID_ARG_TYPE'
        }

--- src/functions.js
+++ src/functions.js
@@ -4078,7 +4078,7 @@ function save_selection_to_file() {
 	if (selection && selection.canvas) {
 		systemHooks.showSaveFileDialog({
 			dialogTitle: localize("Save As"),
-			defaultName: "selection.png",
+			defaultFileName: "selection.png",
 			defaultFileFormatID: "image/png",
 			formats: image_formats,
 			getBlob: (new_file_type) => {
