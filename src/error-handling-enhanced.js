dafef7b1b4f2d33c32c434b1e1fe0ffbc271600d
Isaiah Odhner <isaiahodhner@gmail.com>
2024-05-22 17:36:22 -0400

    Handle errors with no stack trace, like SyntaxError
    Show URL and line/column numbers for errors with no call stack.

--- src/error-handling-enhanced.js
+++ src/error-handling-enhanced.js
@@ -16,9 +16,26 @@ import { show_error_message } from "./functions.js";
 var old_onerror = window.onerror;
 window.onerror = function (message, source, lineno, colno, error) {
 	try {
-		// Some errors don't give an error object, like "ResizeObserver loop limit exceeded"
-		// @ts-ignore
-		show_error_message(localize("Internal application error."), error || message);
+		/** @type {Error | string} */
+		var error_details = "";
+		if (!error) {
+			// Some errors don't give an error object, like "ResizeObserver loop limit exceeded"
+			// in which case the message should be used.
+			// The source/lineno/colno arguments are not helpful in this case.
+			// To test this: copy code (excluding onerror handler) from this nice repo: https://github.com/OliverJAsh/resize-observer-loop-tests/blob/6a9a5bd8e443c38d3df63833fc7e04abfc6fec32/simple.html#L8-L25
+			// (BTW: I could specifically check that `source === location.href` to ignore the source/lineno/colno arguments, and otherwise show them...)
+			error_details = /** @type {string} */(message); // This is a string, never an Event. Event is for onerror of images etc.
+		} else if ("stack" in error && error.stack.indexOf(source) === -1) {
+			// Some errors give an error object without a stack trace, like "SyntaxError: Unexpected reserved word",
+			// but the source/lineno/colno are actually available.
+			// There's no stack trace because it hasn't started executing yet.
+			// Still it would be nice if they gave a "fake" stack trace with the source/lineno/colno,
+			// so we don't have to handle this special case.
+			error_details = `${message}\n\nURL: ${source}\nLine: ${lineno}\nColumn: ${colno}`;
+		} else {
+			error_details = error;
+		}
+		show_error_message(localize("Internal application error."), error_details);
 	} catch (e) {
 		old_onerror(message, source, lineno, colno, error);
 		console.warn("Error in error handler:", e);
