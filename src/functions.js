1j01/jspaint

commit 22bd88e851e1c0553a5a11983c29f32c527716ce

Fix lint and spellcheck errors

src/functions.js
@@ -1976,9 +1976,8 @@ function render_history_as_gif() {
 /**
  * @param {HistoryNode} target_history_node
  * @param {boolean=} canceling
- * @param {boolean=} discard_document_state
  */
-function go_to_history_node(target_history_node, canceling, discard_document_state) {
+function go_to_history_node(target_history_node, canceling) {
 	const from_history_node = current_history_node;
 
 	if (!target_history_node.image_data) {
@@ -2674,7 +2673,7 @@ function image_invert_colors() {
 	apply_image_transformation({
 		name: localize("Invert Colors"),
 		icon: get_help_folder_icon("p_invert.png"),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (_original_canvas, original_ctx, _new_canvas, new_ctx) => {
 		const monochrome_info = monochrome && detect_monochrome(original_ctx);
 		if (monochrome && monochrome_info.isMonochrome) {
 			invert_monochrome(original_ctx, new_ctx, monochrome_info);
@@ -2793,7 +2792,7 @@ function view_bitmap() {
 		// but it also doesn't do anything else — other than changing the cursor. Stupid.
 		cleanup_bitmap_view();
 	}
-	function onMouseDown(event) {
+	function onMouseDown(_event) {
 		// Note: in mspaint, only left click exits View Bitmap mode.
 		// Right click can show a useless context menu.
 		cleanup_bitmap_view();
@@ -3157,7 +3156,7 @@ function image_attributes() {
 	const $table = $(E("table")).appendTo($main);
 	for (const k in table) {
 		const $tr = $(E("tr")).appendTo($table);
-		const $key = $(E("td")).appendTo($tr).text(k);
+		// const $key = $(E("td")).appendTo($tr).text(k);
 		const $value = $(E("td")).appendTo($tr).text(table[k]);
 		if (table[k].indexOf("72") !== -1) {
 			$value.css("direction", "ltr");
@@ -4025,7 +4024,7 @@ function read_image_file(blob, callback) {
 					// @ts-ignore
 					error.code = file_text.match(/^\s*<!doctype\s+html/i) ? "html-not-image" : "decoding-failure";
 					callback(error);
-				}, (err) => {
+				}, (_err) => {
 					const error = new Error("failed to decode blob as image or text");
 					// @ts-ignore
 					error.code = "decoding-failure";

.eslintrc.js
@@ -110,7 +110,12 @@ module.exports = {
 			parserOptions: { sourceType: "module" },
 			rules: {
 				"no-undef": "warn",
-				"no-unused-vars": "error",
+				"no-unused-vars": ["warn", {
+					"args": "all",
+					"argsIgnorePattern": "^_",
+					"caughtErrorsIgnorePattern": "^_",
+					// "varsIgnorePattern": "^_",
+				}],
 			},
 		}
 	]

src/app.js
@@ -1438,7 +1438,7 @@ $canvas.on("pointerleave", (e) => {
 // #region Panning and Zooming
 let last_zoom_pointer_distance;
 let pan_last_pos;
-let pan_start_magnification; // for panning and zooming in the same gesture
+// let pan_start_magnification; // for panning and zooming in the same gesture (...was this ever used?)
 let first_pointer_time;
 const discard_quick_undo_period = 500; // milliseconds in which to treat gesture as just a pan/zoom if you use two fingers, rather than treating it as a brush stroke you might care about
 function average_points(points) {
@@ -1485,7 +1485,7 @@ $canvas_area.on("pointerdown", (event) => {
 	if (pointers.length == 2) {
 		last_zoom_pointer_distance = Math.hypot(pointers[0].x - pointers[1].x, pointers[0].y - pointers[1].y);
 		pan_last_pos = average_points(pointers);
-		pan_start_magnification = magnification;
+		// pan_start_magnification = magnification;
 	}
 	// Quick Undo when there are multiple pointers (i.e. for touch)
 	// See pointermove for other pointer types

src/discord-activity-client.js
@@ -69,12 +69,15 @@ export function handleExternalLinks() {
 	document.addEventListener('click', (e) => {
 		if (e.defaultPrevented) return;
 		const target = e.target;
-		if (target.tagName === 'A' && target.href && target.target === '_blank') {
+		if (target instanceof HTMLAnchorElement && target.href && target.target === '_blank') {
 			e.preventDefault();
 			discordSdk.commands.openExternalLink({ url: target.href });
 		}
 	});
-	window.open = (url) => discordSdk.commands.openExternalLink({ url });
+	window.open = (url) => {
+		discordSdk.commands.openExternalLink({ url });
+		return null;
+	};
 }
 
 export async function shareImage(blob, filename) {

src/extra-tools.js
@@ -62,7 +62,7 @@ const extra_tools = [{
 		x: 0,
 		y: 0,
 	},
-	pointerdown(ctx, x, y) {
+	pointerdown(_ctx, x, y) {
 		this.position.x = x;
 		this.position.y = y;
 		this.velocity.x = 0;
@@ -111,7 +111,7 @@ const extra_tools = [{
 	description: "Lets you configure the Airbrushbrush. It uses this type of tool option as well.",
 	cursor: ["airbrush", [7, 22], "crosshair"],
 	continuous: "time",
-	paint(ctx, x, y) {
+	paint(_ctx, _x, _y) {
 
 	},
 	$options: $choose_airbrush_size

src/globals.d.ts
@@ -1005,3 +1005,26 @@ interface HTMLElement {
 	mozRequestFullScreen?: typeof HTMLElement.prototype.requestFullscreen;
 	msRequestFullscreen?: typeof HTMLElement.prototype.requestFullscreen;
 }
+
+// Discord Embedded App SDK
+// Copied from:
+// https://github.com/discord/embedded-app-sdk/blob/49d23756144ebf36a5735bd7f161aae6cb359939/examples/react-colyseus/packages/client/src/types.tsx#L13C1-L31C2
+interface IGuildsMembersRead {
+	roles: string[];
+	nick: string | null;
+	avatar: string | null;
+	premium_since: string | null;
+	joined_at: string;
+	is_pending: boolean;
+	pending: boolean;
+	communication_disabled_until: string | null;
+	user: {
+		id: string;
+		username: string;
+		avatar: string | null;
+		discriminator: string;
+		public_flags: number;
+	};
+	mute: boolean;
+	deaf: boolean;
+}

src/image-manipulation.js
@@ -803,7 +803,7 @@ function flip_horizontal() {
 	apply_image_transformation({
 		name: localize("Flip horizontal"),
 		icon: get_help_folder_icon("p_fliph.png"),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (original_canvas, _original_ctx, new_canvas, new_ctx) => {
 		new_ctx.translate(new_canvas.width, 0);
 		new_ctx.scale(-1, 1);
 		new_ctx.drawImage(original_canvas, 0, 0);
@@ -814,7 +814,7 @@ function flip_vertical() {
 	apply_image_transformation({
 		name: localize("Flip vertical"),
 		icon: get_help_folder_icon("p_flipv.png"),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (original_canvas, _original_ctx, new_canvas, new_ctx) => {
 		new_ctx.translate(0, new_canvas.height);
 		new_ctx.scale(1, -1);
 		new_ctx.drawImage(original_canvas, 0, 0);
@@ -830,7 +830,7 @@ function rotate(angle) {
 	apply_image_transformation({
 		name: `${localize("Rotate by angle")} ${angle / TAU * 360} ${localize("Degrees")}`,
 		icon: get_help_folder_icon(`p_rotate_${angle >= 0 ? "cw" : "ccw"}.png`),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (original_canvas, _original_ctx, new_canvas, new_ctx) => {
 		new_ctx.save();
 		switch (angle) {
 			case TAU / 4:
@@ -922,7 +922,7 @@ function stretch_and_skew(x_scale, y_scale, h_skew, v_skew) {
 						(x_scale !== 1) ? "p_stretch_both.png" : "p_stretch_v.png"
 					) : "p_stretch_h.png"
 		),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (original_canvas, _original_ctx, new_canvas, new_ctx) => {
 		const w = original_canvas.width * x_scale;
 		const h = original_canvas.height * y_scale;
 

src/repack-spritesheet.js
@@ -21,7 +21,7 @@ function repack_spritesheet(tile_width, tile_height = tile_width, padding = tile
 		name: localize("Repack Spritesheet"),
 		icon: get_help_folder_icon("p_paste.png"),
 		// icon: get_help_folder_icon("p_stretch_both.png"),
-	}, (original_canvas, original_ctx, new_canvas, new_ctx) => {
+	}, (original_canvas, _original_ctx, new_canvas, new_ctx) => {
 		const tiles_x = Math.ceil(original_canvas.width / tile_width);
 		const tiles_y = Math.ceil(original_canvas.height / tile_height);
 		new_canvas.width = tiles_x * (tile_width + padding) + padding;

commit af94876a4ffef95c2d00c0022d89a452034793b7

Fix missing attribute labels in Image > Attributes dialog
Broken in https://github.com/1j01/jspaint/commit/22bd88e851e1c

I mistakenly commented this out to avoid a lint error about an unused
variable, apparently ignoring the side effect that this line has.

src/functions.js
@@ -3161,7 +3161,7 @@ function image_attributes() {
 	const $table = $(E("table")).appendTo($main);
 	for (const k in table) {
 		const $tr = $(E("tr")).appendTo($table);
-		// const $key = $(E("td")).appendTo($tr).text(k);
+		$(E("td")).appendTo($tr).text(k);
 		const $value = $(E("td")).appendTo($tr).text(table[k]);
 		if (table[k].indexOf("72") !== -1) {
 			$value.css("direction", "ltr");