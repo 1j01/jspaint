947782c00c13c3785fe0f5a97871acdc22ab5ccc
Isaiah Odhner <isaiahodhner@gmail.com>
2024-06-02 18:50:38 -0400

    Enable ESLint Stylistic rules that don't require code changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,6 @@ import globals from "globals";
 /** @type {import('@types/eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
-	// stylistic.configs.customize({
-	// 	indent: "tab",
-	// 	quotes: 'double',
-	// 	semi: true,
-	// 	jsx: false,
-	// 	braceStyle: '1tbs',
-	// 	arrowParens: 'always',
-	// 	commaDangle: 'always-multiline',
-	// }),
 	{
 		"linterOptions": {
 			"reportUnusedDisableDirectives": "warn",
@@ -88,10 +79,13 @@ export default [
 			"no-restricted-globals": ["error", "event", "canvas", "ctx", "colors", "i", "j", "k", "x", "y", "z", "width", "height", "w", "h"],
 
 			// Stylistic:
-			// "@stylistic/array-bracket-spacing": "error",
-			// "@stylistic/block-spacing": "error",
-			// "@stylistic/padded-blocks": "off",
-			// "@stylistic/brace-style": "error",
+			"@stylistic/array-bracket-newline": ["error", "consistent"],
+			"@stylistic/array-bracket-spacing": ["error", "never"],
+			// "@stylistic/array-element-newline": ["error", "consistent"], // lot of big arrays with sometimes meaningful line breaks; could exclude certain files though
+			// "@stylistic/arrow-parens": ["error", "always"], // TODO
+			"@stylistic/arrow-spacing": ["error", { "before": true, "after": true }],
+			"@stylistic/block-spacing": ["error", "always"],
+			// "@stylistic/brace-style": ["error", "1tbs", { "allowSingleLine": true }], // TODO
 			"@stylistic/comma-dangle": ["error", {
 				"arrays": "always-multiline", // ensure commas to avoid confusing git diffs
 				"objects": "always-multiline", // ensure commas to avoid confusing git diffs
@@ -99,6 +93,101 @@ export default [
 				"exports": "never", // matches VS Code's default JS/TS formatter
 				"functions": "only-multiline", // commas sometimes avoid confusing git diffs, sometimes are confusing themselves
 			}],
+			// "@stylistic/comma-spacing": ["error", { "before": false, "after": true }], // TODO
+			"@stylistic/comma-style": ["error", "last"],
+			"@stylistic/computed-property-spacing": ["error", "never"],
+			"@stylistic/dot-location": ["error", "property"],
+			// "@stylistic/eol-last": ["error", "always"], // TODO
+			// "@stylistic/function-call-argument-newline": ["error", "consistent"], // several places with meaningful line breaks grouping arguments
+			"@stylistic/function-call-spacing": ["error", "never"],
+			// "@stylistic/function-paren-newline": ["error", "multiline-arguments"], // several places with meaningful line breaks grouping arguments
+			"@stylistic/generator-star-spacing": ["error", "after"],
+			// "@stylistic/implicit-arrow-linebreak": ["error", "beside"], // could encourage parens for clarity, but ESLint won't mention that's the reason, so probably not a good idea
+			// "@stylistic/indent": ["error", "tab"], // TODO: there's some conflicts with VS Code's formatter; could investigate the options here or perhaps configure VS Code to use ESLint for formatting
+			// "@stylistic/indent-binary-ops": ["error", "tab"], // TODO (conflicts with VS Code's formatter)
+			// "@stylistic/jsx-child-element-spacing": "off",
+			// "@stylistic/jsx-closing-bracket-location": "off",
+			// "@stylistic/jsx-closing-tag-location": "off",
+			// "@stylistic/jsx-curly-brace-presence": "off",
+			// "@stylistic/jsx-curly-newline": "off",
+			// "@stylistic/jsx-curly-spacing": "off",
+			// "@stylistic/jsx-equals-spacing": "off",
+			// "@stylistic/jsx-first-prop-new-line": "off",
+			// "@stylistic/jsx-function-call-newline": "off",
+			// "@stylistic/jsx-indent": "off",
+			// "@stylistic/jsx-indent-props": "off",
+			// "@stylistic/jsx-max-props-per-line": "off",
+			// "@stylistic/jsx-newline": "off",
+			// "@stylistic/jsx-one-expression-per-line": "off",
+			// "@stylistic/jsx-pascal-case": "off",
+			// "@stylistic/jsx-props-no-multi-spaces": "off",
+			// "@stylistic/jsx-quotes": "off",
+			// "@stylistic/jsx-self-closing-comp": "off",
+			// "@stylistic/jsx-sort-props": "off",
+			// "@stylistic/jsx-tag-spacing": "off",
+			// "@stylistic/jsx-wrap-multilines": "off",
+			"@stylistic/key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+			"@stylistic/keyword-spacing": ["error", { "before": true, "after": true }],
+			// "@stylistic/line-comment-position": "off",
+			// "@stylistic/linebreak-style": "off",
+			// "@stylistic/lines-around-comment": ["error", ""], // TODO (so many options...)
+			"@stylistic/lines-between-class-members": ["error", "never"],
+			// "@stylistic/max-len": ["error", ""], // TODO
+			"@stylistic/max-statements-per-line": ["error", { "max": 4 }], // TODO: maybe decrease this
+			// TODO: lint .d.ts files, and ideally JSDoc comments
+			// "@stylistic/member-delimiter-style": ["error", { "multiline": { "delimiter": "semi", "requireLast": true }, "singleline": { "delimiter": "semi", "requireLast": true } }],
+			// "@stylistic/multiline-comment-style": ["error", "separate-lines"], // I use block comments sometimes for disabled code; also this is detecting a few JSDoc comments even though it says it won't without "checkJSDoc" set to true.
+			// "@stylistic/multiline-ternary": ["error", "always-multiline"], // TODO maybe
+			// "@stylistic/new-parens": ["error", "always"], // TODO
+			"@stylistic/newline-per-chained-call": ["error", { "ignoreChainWithDepth": 5 }], // TODO: maybe decrease this
+			// "@stylistic/no-confusing-arrow": ["error"], // TODO: just two cases to look at
+			// "@stylistic/no-extra-parens": "off", // if there are extra parens, it's probably for clarity (TODO: look at the granular options)
+			"@stylistic/no-extra-semi": "error",
+			"@stylistic/no-floating-decimal": "error",
+			// "@stylistic/no-mixed-operators": ["error", ...], // TODO: investigate options
+			// "@stylistic/no-mixed-spaces-and-tabs": "error", // TODO
+			// "@stylistic/no-multi-spaces": "error", // TODO
+			// "@stylistic/no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }], // TODO
+			// "@stylistic/no-tabs": "off", // I use tabs for indentation, in disabled code, and in template literals
+			// "@stylistic/no-trailing-spaces": "error", // TODO
+			"@stylistic/no-whitespace-before-property": "error",
+			"@stylistic/nonblock-statement-body-position": ["error", "beside"], // TODO: maybe drop curly braces from single line conditionals
+			// "@stylistic/object-curly-newline": ["error", ""], // TODO: investigate options
+			"@stylistic/object-curly-spacing": ["error", "always"],
+			// "@stylistic/object-property-newline": ["error", { "allowAllPropertiesOnSameLine": true }], // TODO
+			// "@stylistic/one-var-declaration-per-line": ["error", "always"], // TODO maybe? (too bad there's no "usually" option, haha)
+			// "@stylistic/operator-linebreak": ["error", "after"], // TODO maybe, there's just a few exceptions currently
+			// "@stylistic/padded-blocks": "off", // it depends, especially on how much stuff is in the block
+			// "@stylistic/padding-line-between-statements": ["error", ""], // TODO: investigate options, looks very complex
+			// "@stylistic/quote-props": ["error", "consistent"], // TODO probably
+			// "@stylistic/quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }], // TODO maybe
+			"@stylistic/rest-spread-spacing": ["error", "never"],
+			// "@stylistic/semi": ["error", "always"], // TODO
+			"@stylistic/semi-spacing": ["error", { "before": false, "after": true }],
+			"@stylistic/semi-style": ["error", "last"],
+			"@stylistic/space-before-blocks": ["error", "always"],
+			"@stylistic/space-before-function-paren": ["error", {
+				"anonymous": "always",
+				"named": "never",
+				"asyncArrow": "always"
+			}],
+			"@stylistic/space-in-parens": ["error", "never"],
+			"@stylistic/space-infix-ops": "error",
+			// TODO: one file actually uses spaces between unary operators _artistically_; could disable for that file (helpers.js)
+			// "@stylistic/space-unary-ops": ["error", {
+			// 	"words": true,
+			// 	"nonwords": false,
+			// }],
+			// "@stylistic/spaced-comment": ["error", "always"], // TODO: investigate
+			"@stylistic/switch-colon-spacing": ["error", { "after": true, "before": false }],
+			"@stylistic/template-curly-spacing": ["error", "never"],
+			"@stylistic/template-tag-spacing": ["error", "never"],
+			// "@stylistic/type-annotation-spacing": ["error", ""], // TODO: lint TS
+			// "@stylistic/type-generic-spacing": ["error", ""], // TODO: lint TS
+			// "@stylistic/type-named-tuple-spacing": ["error", ""], // TODO: lint TS
+			"@stylistic/wrap-iife": ["error", "inside"],
+			// "@stylistic/wrap-regex": "error", // does that really clarify things? eh
+			"@stylistic/yield-star-spacing": ["error", "after"],
 		},
 	},
 	{
